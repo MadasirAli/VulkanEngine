1edf3cf4d16034a4478e5947a5b847d628f3d235
Madasir Ali <madasirali672@gmail.com>
2023-11-15 15:42:44 +0500

    shaders compilation added.

---
+++ compile_shaders.bat
@@ -0,0 +1,2 @@
+glslc -fshader-stage=vertex shaders\vertex_shader.glsl -o x64\Debug\vertex_shader.spv
+glslc -fshader-stage=fragment shaders\fragment_shader.glsl -o x64\Debug\fragment_shader.spv
--- shaders/fragmentshader.glsl
+++
@@ -1,9 +0,0 @@
-# version 450
-
-layout (location = 0) out outColor;
-layout (location = 0) in fragColor;
-
-void main()
-{
-	outColor = vec4(fragColor, 1.0);
-}
